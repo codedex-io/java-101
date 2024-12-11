f37710c35b6cba8b549908f1b8d616abe6bf43e6
estrella <epopo2@illinois.edu>
2024-12-10 19:55:49 -0600

    Edit oatmeal

--- 3-control-flow/11-oatmeal.java
+++ 3-control-flow/11-oatmeal.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-      boolean oats = true;
+  public static void main(String[] args) {
+      int oats = 2;
 
-      if (oats) {
-        System.out.println("Let's make oatmeal today!");
+      if (oats > 0) {
+          System.out.println("Let's make oatmeal today!");
       } 
-    }
+  }
 }

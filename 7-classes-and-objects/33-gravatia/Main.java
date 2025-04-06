b4c4e604888e7ce53734cabc94b3bdc8a1594733
Sonny Li <sonnynomnom@gmail.com>
2025-04-06 03:47:41 -0400

    Update Main.java

--- 7-classes-and-objects/33-gravatia/Main.java
+++ 7-classes-and-objects/33-gravatia/Main.java
@@ -11,7 +11,7 @@ class Neopet {
 
 public class Main {
     public static void main(String[] args) {
-        // Create a new Neopet object here 💖
+        // Create new Neopet object here 💖
         Neopet gravatia = new Neopet();
 
         // Set the attributes of the Neopet object here 💖
@@ -22,7 +22,7 @@ public class Main {
         gravatia.hasScarf = false;
 
         // Print the attributes of the Neopet 💖
-        System.out.println("Name: "+ gravatia.name);
+        System.out.println("Name: " + gravatia.name);
         System.out.println("Species: " + gravatia.species);
         System.out.println("Color: " + gravatia.color);
         System.out.println("Gender: " + gravatia.gender);

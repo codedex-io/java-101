c4fdfadec69b9c032b857361eb761288db676278
Sonny Li <sonnynomnom@gmail.com>
2025-04-13 16:28:55 -0400

    Update 22-position-0.java

--- 5-arrays/22-position-0.java
+++ 5-arrays/22-position-0.java
@@ -3,7 +3,7 @@
 
 public class HelloWorld {
     public static void main(String[] args) {        
-        String[] sodaDrinks = {"Dr.Pepper", "Orange Fanta", "Coke", "Japanese Soda", "Sprite", "Poppi", "Diet Coke", "Diet Dr.Pper"}; 
+        String[] sodaDrinks = {"Dr Pepper", "Orange Fanta", "Coke", "Japanese Soda", "Sprite", "Poppi", "Diet Coke", "Diet Dr Pepper"}; 
 
         System.out.println(sodaDrinks[3]);
         System.out.println(sodaDrinks[4]);

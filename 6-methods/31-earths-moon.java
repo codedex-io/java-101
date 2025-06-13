79056c4e457651d61c3094af1cd691c75408763f
Sonny Li <sonnynomnom@gmail.com>
2025-06-13 19:01:20 -0400

    Update 31-earths-moon.java

--- 6-methods/31-earths-moon.java
+++ 6-methods/31-earths-moon.java
@@ -1,9 +1,13 @@
+// Earth's Moon 🌑
+// Codédex
+
 public class HelloWorld {
 
     public static double earthMoonWeight(String name, int weight, String unit) {
-
+        
         double moonWeight = weight * (1.62 / 9.81);
         return moonWeight;
+        
     }
 
     public static void main(String[] args) {
@@ -12,7 +16,6 @@ public class HelloWorld {
         int earthWeight = 30;
         String unit = "lbs";
         double moonWeight = earthMoonWeight(name, earthWeight, unit);
-
 
         System.out.println("Hi " + name + "! Your weight on Earth is " + earthWeight + unit + 
                            " and your weight on the moon would be about " + moonWeight + unit + ".");

89ac9feb00c52df8c172fb4c3d9b5f122f731e0a
Ellie Popoca <epopo2@illinois.edu>
2025-11-17 02:30:49 -0600

    Update multiply call with new arguments

--- 6-methods/28-tv-channels.java
+++ 6-methods/28-tv-channels.java
@@ -21,7 +21,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("The result is " + multiply(new int[] { 1, 2, 3, 4, 5 }));  
-        System.out.println("The result is " + multiply(13, 8, 2));
+        System.out.println("The result is " + multiply(13, 8, 7));
         System.out.println("The result is " + multiply(7, 9, 3));
         System.out.println("The result is " + multiply(233.9, 0.09));  
         System.out.println("The result is " + multiply(0.1, 0.1));

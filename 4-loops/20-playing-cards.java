439b3bb4355d5d3328c9c0b661a16dc8ae85db6c
markofornoliak <130382040+markofornoliak@users.noreply.github.com>
2026-02-09 19:19:01 +0100

    Refactor card printing logic in HelloWorld
    Removed duplicate card output and fixed incorrect card message.

--- 4-loops/20-playing-cards.java
+++ 4-loops/20-playing-cards.java
@@ -2,12 +2,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Write your code here 💖
         for (int i = 1; i <= 52; i++) {
+            System.out.println("Card " + i);
             if (i == 32) {
-                System.out.println("An incorrect card was found in item " + i);
+                System.out.println("An incorrect card was found in item " + i".");
                 break;
-            } else {
-                System.out.println("Card " + i);
-            }
         }
     }
 }
